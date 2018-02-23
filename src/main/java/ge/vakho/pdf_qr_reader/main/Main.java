fa2c4769861658fa7de21d2125d29012660f2c84
CRA\v.laluashvili <vakho10@gmail.com>
2018-02-23 17:34:39 +0400

    Formatted code.

--- src/main/java/ge/vakho/pdf_qr_reader/main/Main.java
+++ src/main/java/ge/vakho/pdf_qr_reader/main/Main.java
@@ -3,38 +3,39 @@ package ge.vakho.pdf_qr_reader.main;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import com.google.zxing.Result;
 import ge.vakho.pdf_qr_reader.utils.PDFUtils;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    PDDocument document = null;
-    try (InputStream is = Main.class.getResourceAsStream("/Sample-QR-Codes.pdf")) {
+        PDDocument document = null;
+        try (InputStream is = Main.class.getResourceAsStream("/Sample-QR-Codes.pdf")) {
 
-      // (1) Parse PDF document
-      document = PDDocument.load(is);
+            // (1) Parse PDF document
+            document = PDDocument.load(is);
 
-      // (2) Get all the QR results from the document
-      List<Result> results = PDFUtils.getQRResultsFromDocument(document);
+            // (2) Get all the QR results from the document
+            List<Result> results = PDFUtils.getQRResultsFromDocument(document);
 
-      // (3) Process QR results
-      for (Result result : results) {
-        System.out.println(result.getText()); // That's all.        
-      }
+            // (3) Process QR results
+            for (Result result : results) {
+                System.out.println(result.getText()); // That's all.
+            }
 
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      // Oh, god...
-      if (document != null) {
-        try {
-          document.close();
         } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Oh, god...
+            if (document != null) {
+                try {
+                    document.close();
+                } catch (IOException e) {
+                }
+            }
         }
-      }
     }
-  }
 }
